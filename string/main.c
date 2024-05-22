c28f694f8f96b161d2df36e30f4f21875cff8dd7
fedormac <fedormaccc@mail.ru>
2024-05-22 17:24:37 +0300

    Auto commit: Ср 22 мая 2024 17:24:37 MSK

--- string/main.c
+++ string/main.c
@@ -8,6 +8,7 @@ typedef struct {
 size_t s21_str_len(char *str);
 void *s21_memchr(const void *_str, int c, size_t n);
 int s21_memcmp(const void *_str1, const void *_str2, size_t n);
+void *s21_memcpy(void *_dest, const void *_src, size_t n);
 
 /*
 1
@@ -16,20 +17,35 @@ int s21_memcmp(const void *_str1, const void *_str2, size_t n);
 
 */
 int main() {
-  char *str = "abc";
+  char *str = "adgbc";
   char *str2 = "abd";
 
   char *find;
+  find = calloc(6, sizeof(char));
   int i = 0;
   int size, razn;
-  size = s21_str_len(str);
-  printf("%d\n", size);
-  find = s21_memchr(str, ' ', s21_str_len(str));
-  // printf("%ld\n", find - str);
-  razn = s21_memcmp(str, str2, sizeof(str));
-  printf("%d\n", razn);
+  // size = s21_str_len(str);
+  // printf("%d\n", size);
+  // find = s21_memchr(str, ' ', s21_str_len(str));
+  //  printf("%ld\n", find - str);
+  // razn = s21_memcmp(str, str2, sizeof(str));
+  // printf("%d\n", razn);
+  s21_memcpy(find, str, sizeof(str));
+  printf("%s\n", find);
 
   return 0;
+}
+void *s21_memcpy(void *_dest, const void *_src, size_t n) {
+  char *dest = _dest;
+  const char *str = _src;
+  int i = 0;
+  while (i < n + 1) {
+    dest[i] = str[i];
+
+    i++;
+  }
+
+  return (void *)dest;
 }
 size_t s21_str_len(char *str) {
   int i = 0;
--- string/test.c
+++ string/test.c
@@ -2,16 +2,29 @@
 #include <stdlib.h>
 #include <string.h>
 int main() {
-  char block1[] = "abc";
-  char block2[] = "abd";
-  char block3[] = "abc";
+  char src[] = "Привет, мир!";
+  char *dest;
+  // dest=calloc(20,sizeof(char));
 
-  int result1 = memcmp(block1, block2, sizeof(block1));
-  int result2 = memcmp(block1, block3, sizeof(block1));
-  printf("%ld\n", sizeof(block1));
-  printf("Result 1: %d\n", result1);  // Вернет 0, так как блоки идентичны
-  printf("Result 2: %d\n",
-         result2);  // Вернет отрицательное число, так как block1 меньше block3
+  memcpy(dest, src, sizeof(src));  // Копируем всю строку src в dest
+
+  printf("Копированная строка: %s\n", dest);
 
   return 0;
 }
+
+// int main() {
+//   char block1[] = "abc";
+//   char block2[] = "abd";
+//   char block3[] = "abc";
+
+//   int result1 = memcmp(block1, block2, sizeof(block1));
+//   int result2 = memcmp(block1, block3, sizeof(block1));
+//   printf("%ld\n", sizeof(block1));
+//   printf("Result 1: %d\n", result1);  // Вернет 0, так как блоки идентичны
+//   printf("Result 2: %d\n",
+//          result2);  // Вернет отрицательное число, так как block1 меньше
+//          block3
+
+//   return 0;
+// }

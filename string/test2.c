19f0bb72e0113c8225a703e9c0d756f5fe9a06b9
fedormac <fedormaccc@mail.ru>
2024-06-15 18:19:50 +0300

    Auto commit: Сб 15 июн 2024 18:19:50 MSK

--- string/test2.c
+++ string/test2.c
@@ -5,19 +5,5 @@ int main() {
   double divisor = 5.0;
   double remainder;
 
-  // Вычисление остатка от деления
-  remainder = fmod(dividend, 10);
-  //  printf("%f ",remainder);
-  int co = 0;
-  while (remainder != .0) {
-    remainder *= 10;
-
-    printf("%f ", remainder);
-    co++;
-  }
-
-  printf("Остаток от деления %.2f на %.2f равен %f %d\n", dividend, divisor,
-         remainder, co);
-
   return 0;
 }
